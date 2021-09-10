0d0cc61f3c0987f697277261ad5f9565246a62bc
Vatsal Manot <vatsal.manot@yahoo.com>
2021-09-11 04:15:06 +0530

    Update package

--- Sources/Intramodular/Coordinator.swift
+++ Sources/Intramodular/Coordinator.swift
@@ -11,7 +11,7 @@ public struct Coordinator<WrappedValue: ViewCoordinator>: DynamicProperty, Prope
     
     @OptionalEnvironmentObject public var _wrappedValue0: WrappedValue?
     @OptionalEnvironmentObject public var _wrappedValue1: AnyViewCoordinator<WrappedValue.Route>?
-        
+    
     @inline(never)
     public var wrappedValue: WrappedValue {
         let result: Any? = nil
@@ -19,11 +19,11 @@ public struct Coordinator<WrappedValue: ViewCoordinator>: DynamicProperty, Prope
             ?? _wrappedValue1?.base
             ?? OpaqueBaseViewCoordinator
             ._runtimeLookup[ObjectIdentifier(WrappedValue.self)]?.takeUnretainedValue()
-
+        
         guard let result = result else {
             fatalError("Could not resolve a coordinator for \(String(describing: WrappedValue.Route.self)) in the view hierarchy. Try adding `.coordinator(myCoordinator)` in your view hierarchy.")
         }
-
+        
         if let result = result as? EnvironmentProvider {
             result.environmentBuilder.merge(environmentBuilder)
         }
@@ -36,13 +36,13 @@ public struct Coordinator<WrappedValue: ViewCoordinator>: DynamicProperty, Prope
     }
     
     public init<Route>(for route: Route.Type) where WrappedValue == AnyViewCoordinator<Route> {
-
+        
     }
     
     public init<Route>(
         _ route: Route.Type
     ) where WrappedValue == AnyViewCoordinator<Route> {
-
+        
     }
 }
 
@@ -58,7 +58,7 @@ extension View {
                     coordinator.rootViewController = $0
                 }
             }
-
+        
     }
     
     public func coordinator<C: UIWindowCoordinator<Route>, Route>(
